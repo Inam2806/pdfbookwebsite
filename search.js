8fddb0328185e3e5acc7eb43ea4770a34b165ecd
INAM UL HAQUE <100591641+Inam2806@users.noreply.github.com>
2022-07-07 15:43:15 +0530

    Add files via upload

---
+++ search.js
@@ -0,0 +1,17 @@
+function searchfun(){
+    let filter = document.getElementById('myInput').value.toUpperCase();
+    let Books =document.getElementById('Books');
+    let tr = Books.getElementsByTagName('tr');
+    for(var i=0;i<tr.length;i++){
+        let td =tr[i].getElementsByTagName('td')[0];
+        if(td){
+            let textvalue =td.textContent  || td.innerHTML;
+            if(textvalue.toUpperCase().indexOf(filter) >-1){
+                tr[i].style.display ="";
+            }
+            else{
+                tr[i].style.display ="none";
+            }
+        }
+    }
+}
